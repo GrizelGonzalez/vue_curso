105d231afdebb755578a812436676c3a4232fa3a
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-11-24 22:02:22 -0600

    vue y mi primer componente

---
+++ vue_cli/generador_notas/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ something }}</div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      something: "Mi pedodo"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
